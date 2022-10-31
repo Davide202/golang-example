94aa1fcef79912adc33e0c8e4992593bc66d8ed7
Davide D'Innocente <D.DInnocente.esterno@almaviva.it>
2022-10-31 01:26:16 +0100

    add gitignore

--- singers/repository/SingerRepository.go
+++ singers/repository/SingerRepository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"errors"
 	"log"
+	"os"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	//"github.com/google/uuid"
@@ -12,6 +13,14 @@ var dsn string = "root:admin@tcp(127.0.0.1:3306)/recordings?charset=utf8mb4&pars
 type Album struct {
 	//gorm.Model
 	ID int64 `gorm:"primaryKey,column:id"`
+	//APIAlbum 
+	Title string `gorm:"column:title"`
+	Artist string `gorm:"column:artist"`
+	Price float32 `gorm:"column:price"`
+}
+type APIAlbum struct {
+	//gorm.Model
+	//ID int64 `gorm:"primaryKey,column:id"`
 	Title string `gorm:"column:title"`
 	Artist string `gorm:"column:artist"`
 	Price float32 `gorm:"column:price"`
@@ -26,6 +35,8 @@ type Tabler interface {
   }
 
 func conn() (gorm.DB,error){
+	user := os.Getenv("DB_USER")
+	log.Println(user)
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	//dsn := "root:admin@tcp(127.0.0.1:3306)/recordings?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -48,7 +59,7 @@ func (u *Album) BeforeCreate(tx *gorm.DB) (err error) {
 	//u.Title = uuid.New().String()
 	//u.Artist = uuid.New().String()
 	return
-  }
+}
 
 func AlbumsByArtist(artist string)([]Album,error){
 	db , err := conn()
@@ -57,7 +68,9 @@ func AlbumsByArtist(artist string)([]Album,error){
 	}
 	var result []Album
 	//cb := db.ClauseBuilders
-	rows, err := db.Model(&Album{}).Where("artist LIKE ?",artist).Select("*").Rows()
+	const per string = "%"
+	param := per + artist + per
+	rows, err := db.Model(&Album{}).Where("artist LIKE ?",param).Find(&APIAlbum{}).Rows()
 	if err != nil {
 		return nil,err
 	}
