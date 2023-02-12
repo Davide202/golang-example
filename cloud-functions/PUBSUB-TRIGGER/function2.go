147cb3edf2a83ea8f1cd7c6225978511aa4ce358
Davide D'Innocente <D.DInnocente.esterno@almaviva.it>
2023-02-12 22:15:41 +0100

    pub sub triggered func fix

---
+++ cloud-functions/PUBSUB-TRIGGER/function2.go
@@ -0,0 +1,115 @@
+package function
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/cloudevents/sdk-go/v2/event"
+)
+
+type User struct {
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+	Info    string `json:"info"`
+}
+
+type Mess struct {
+	Messag Messag `json:"message"`
+}
+
+type Messag struct {
+	Type        string `json:"@type"`
+	Attrib      Attrib `json:"attributes"`
+	Data        []byte `json:"data"`
+	MessageId   string `json:"messageId"`
+	PublishTime string `json:"publishTime"`
+}
+type Attrib struct {
+	Origin   string `json:"origin"`
+	Username string `json:"username"`
+}
+
+func (u User) String() string {
+	return fmt.Sprintf("\nName: %v, \nSurname: %v", u.Name, u.Surname)
+}
+
+func init() {
+	functions.CloudEvent("HelloPubSub", helloPubSub)
+}
+
+// helloPubSub consumes a CloudEvent message and extracts the Pub/Sub message.
+func helloPubSub(ctx context.Context, e event.Event) error {
+	var err error
+	var user User
+
+	log.Printf("Received Message from Pub/Sub")
+
+	/*
+		//VERSION 1
+
+		log.Printf("Received: %v", e.String())
+		log.Printf("ID: %v", e.ID())
+		log.Printf("Extensions: %v", e.Extensions())
+		log.Printf("Data: %v", e.Data())
+
+		mes := string(e.Data())
+		log.Printf("mess: %v", mes)
+		//var msg pubsub.Message
+		var mess Mess
+		if err = json.Unmarshal(e.Data(), &mess); err != nil {
+			log.Printf("json.Unmarshal(e.Data(), &msg): %v", err)
+			//return fmt.Errorf("event.DataAs: %v", err)
+		}
+		log.Printf("msg %v:", mess.Messag.Data)
+		if err = json.Unmarshal(mess.Messag.Data, &user); err != nil {
+			log.Printf("json.Unmarshal(msg.Data, &user): %v", err)
+			//return fmt.Errorf("event.DataAs: %v", err)
+		}
+		log.Printf("User: %v", user.String())
+	*/
+	/**/
+	// VERSION 2
+	//topicID := os.Getenv("TOPIC")
+	projectID := os.Getenv("PROJECT_ID")
+	subscriptionID := os.Getenv("SUBSCRIPTION")
+	log.Printf("Project: %v - Subscription: %v \n", projectID, subscriptionID)
+
+	//ctx = context.Background()
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Printf("Error creating client: %v\n", err)
+	}
+	log.Print("created client")
+	sub := client.Subscription(subscriptionID)
+	config, err := sub.Config(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(config)
+	if err != nil {
+		log.Printf("Error creating subscription: %v\n", err)
+	}
+	log.Print("created subscription")
+	err = sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
+		log.Print("Handling pub/sub message")
+		log.Printf("Got message: %s", m.Data)
+		err = json.Unmarshal(m.Data, &user)
+		if err != nil {
+			log.Printf("Error Unmashal User %v", err.Error())
+			m.Nack()
+		}
+		log.Printf("User: %v", user.String())
+		m.Ack()
+	})
+	if err != context.Canceled {
+		log.Println("Error Receiving message " + err.Error())
+	}
+
+	return nil
+}
